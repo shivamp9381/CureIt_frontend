14a9444c5b682a11b93b468b656ebacc5a53ca4a
shivamp9381 <shivamp9381@gmail.com>
2024-10-20 12:02:35 +0530

    Footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import  React from 'react';
+import Home from   './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import SignUp from  './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
 
-     {/* Write your code here */}
-   
+          <Route
+            exact path="/" element={<Home />}
+          />
+
+          <Route
+            exact path="/login" element={<Login />}
+          />
+
+          <Route
+            exact path="/signup" element={<SignUp />}
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
