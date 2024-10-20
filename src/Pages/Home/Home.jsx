6cd157cc6fcd8fee39b73ca00ebf7c58348f48a2
shivamp9381 <shivamp9381@gmail.com>
2024-10-20 11:27:35 +0530

    Folder structure craeted

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './Home.module.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Footer'
+import About from '../../Components/About/About'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <About/>
         <Footer/>
     </div>
   )
