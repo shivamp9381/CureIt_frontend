a0a90d56bd8544d558f7801313af0a08f07d119d
shivamp9381 <shivamp9381@gmail.com>
2024-10-24 21:41:14 +0530

    Benefits and Bottom banner created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Footer'
 import BottomBanner from '../../Components/BottomBanner/BottomBanner'
+import Hero from '../../Components/Hero/Hero'
+import HomeCards from '../../Components/HomeCards/HomeCards'
+import Benefits from '../../Components/Benefits/Benefits'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        <About/>
+        <Hero/>
+        <HomeCards/>
+        <Benefits/>
         <BottomBanner/>
         <Footer/>
     </div>
