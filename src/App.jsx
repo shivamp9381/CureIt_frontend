7082bd7e6f45ec1303794d6cabc718881a6b1a5d
shivamp9381 <shivamp9381@gmail.com>
2025-05-18 12:46:53 +0530

    Testing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Home from './Pages/Home/Home';
 import Profile from "./Pages/Profile/Profile"; 
